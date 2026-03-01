470f2092763e3ccdf334ccb4cec81e6c4929abcd
Evgenii Vorobev <justlikethat@Evgeniis-Air.dlink>
2026-03-01 21:29:08 +0300

    write w2v custom

--- main.py
+++ main.py
@@ -1,101 +1,75 @@
-import os
-from typing import List
+import random
+import numpy as np
+with open('/Users/justlikethat/langgraph-course/resourses/artical_web_rag.txt','r') as file:
+    text = " ".join(line.rstrip() for line in file)
 
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain_chroma import Chroma
-from langchain_core.documents import Document
-from langchain_core.messages import HumanMessage, SystemMessage
+s = text
 
-from src.sandbox.settings import settings
+def rand_vec(dim):
+    return np.random.uniform(-3.0, 3.0, size=dim)
 
-URL = "https://docs.langchain.com/oss/python/langchain/rag#rag-chains"
-PERSIST_DIRECTORY = "./db/chroma_db"
-BASE_URL = "https://api.proxyapi.ru/openai/v1"
+def logsumexp(x):
+    m = np.max(x)
+    return m + np.log(np.sum(np.exp(x - m)))
 
-def load_documents(url: str) -> List[Document]:
-    loader = WebBaseLoader(url)
-    return loader.load()
-
-def split_documents(docs: List[Document]) -> List[Document]:
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=50,
-        add_start_index=True,
-    )
-    return splitter.split_documents(docs)
-
-def get_embeddings() -> OpenAIEmbeddings:
-    return OpenAIEmbeddings(
-        api_key=settings.OPENAI_API_KEY,
-        base_url=BASE_URL,
-    )
-
-def build_or_load_vectorstore() -> Chroma:
-    embeddings = get_embeddings()
-
-    vector_store = Chroma(
-        persist_directory=PERSIST_DIRECTORY,
-        embedding_function=embeddings,
-        collection_metadata={"hnsw:space": "cosine"},
-    )
-
-    if vector_store._collection.count() == 0:
-        print("Creating new vector store...")
-        documents = load_documents(URL)
-        chunks = split_documents(documents)
-
-        vector_store.add_documents(chunks)
-        vector_store.persist()
-        print(f"Stored {len(chunks)} chunks.")
-
-    else:
-        print("Loaded existing vector store.")
-
-    return vector_store
-
-def run_rag_query(query: str) -> str:
-    db = build_or_load_vectorstore()
-    retriever = db.as_retriever(search_kwargs={"k": 5})
-    relevant_docs = retriever.invoke(query)
-
-    formatted_docs = "\n\n".join(
-        f"Document {i+1}:\n{doc.page_content[:1000]}"
-        for i, doc in enumerate(relevant_docs)
-    )
-
-    prompt = f"""
-        Use ONLY the information from the documents below to answer the question.
-
-        Question:
-        {query}
-
-        Documents:
-        {formatted_docs}
-
-        If the answer is not contained in the documents, say:
-        "I don't have enough information to answer that question based on the provided documents."
-    """
-
-    llm = ChatOpenAI(
-        model="gpt-5.2",
-        temperature=0.1,
-        api_key=settings.OPENAI_API_KEY,
-        base_url=BASE_URL,
-        max_retries=2,
-    )
-
-    messages = [
-        SystemMessage(content="You are a precise RAG assistant."),
-        HumanMessage(content=prompt),
-    ]
-
-    response = llm.invoke(messages)
-    return response.content
-
-
-if __name__ == "__main__":
-    answer = run_rag_query("What is a RAG chain?")
-    print("\nAnswer:\n")
-    print(answer)
+def softmax(x):
+    x = x - np.max(x)
+    e = np.exp(x)
+    return e / e.sum()
+dim = 3
+lr = 0.05
+s = text
+# d[word] = (v_w, u_w)  center vec, context 
+d = {}
+# s is whole corpus
+for tok in s:
+    if tok not in d:
+        d[tok] = (rand_vec(dim), rand_vec(dim))
+vocab = list(d.keys())
+w2i = {w:i for i,w in enumerate(vocab)}
+V = np.stack([d[w][0] for w in vocab], axis=0)  # center vectors (|V|, dim)
+U = np.stack([d[w][1] for w in vocab], axis=0)  # context vectors (|V|, dim
+for _ in range(100):
+    for i in range(len(s) - 5 + 1): 
+        win = s[i:i+5]
+        c_word = win[2]
+        c = w2i[c_word]
+    
+        for j, t_word in enumerate(win):
+            if j == 2: 
+                continue
+            t = w2i[t_word]
+    
+            v_c = V[c]                 # (dim,)
+            z = U @ v_c                # (|V|,)
+            p = softmax(z)             # (|V|,)
+    
+            # g = p - y
+            g = p.copy()
+            g[t] -= 1.0                # (|V|,)
+    
+            # gradients
+            grad_v = U.T @ g           # (dim,)
+            grad_U = np.outer(g, v_c)  # (|V|, dim)
+    
+            # SGD step
+            V[c] -= lr * grad_v
+            U    -= lr * grad_U
+    loss = 0.0
+    pair_count = 0
+    for i in range(len(s) - 5 + 1):
+        win = s[i:i+5]
+        center = win[2]
+        v_center = d[center][0]     # v_w
+        logits = U @ v_center       # (|V|,)  logits for all context words
+        logZ = logsumexp(logits)    # log sum exp over vocab
+        for j, ctx in enumerate(win):
+            if j == 2:
+                continue
+            # log p(ctx | center) = (u_ctx·v_center) - logZ
+            u_ctx = d[ctx][1]
+            loss -= (u_ctx @ v_center) - logZ   # negative log-likelihood
+            pair_count += 1
+    
+    avg_loss = loss / max(pair_count, 1)
+    print("loss:", loss, "avg_loss:", avg_loss, "pairs:", pair_count)
